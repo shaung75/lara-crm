98f703f22ba604293d6b7716b2c3ee5084cddab8
Shaun Gill <shaung75@gmail.com>
2018-10-15 00:00:56 +0100

    Model, Controller and Migration for Customer

---
+++ database/migrations/2018_10_14_224428_create_customers_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('name');
+            $table->string('company');
+            $table->string('address1');
+            $table->string('address2');
+            $table->string('town');
+            $table->string('county');
+            $table->string('postcode');
+            $table->string('telno');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
