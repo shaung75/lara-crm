cdce2a0461f15650016c833f35c2f04d0edf2207
Shaun Gill <shaung75@gmail.com>
2018-11-14 16:11:59 +0000

    Google analytics tracking

--- resources/views/layouts/partials/public/head.blade.php
+++ resources/views/layouts/partials/public/head.blade.php
@@ -28,6 +28,16 @@
     <!-- Stripe integration bits -->
     <script src="https://js.stripe.com/v3/"></script>
 
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-4004309-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-4004309-1');
+    </script>
+
   </head>
 
   <body id="page-top">
