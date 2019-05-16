1564c5a393ffd7b71e9a812f28a061a3fb66c44b
Shaun Gill <shaung75@gmail.com>
2019-05-16 11:15:00 +0100

    Adding invoice no to print view

--- resources/views/dashboard/invoices/print.blade.php
+++ resources/views/dashboard/invoices/print.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>{{ $invoice->customer->company }}</title>
+    <title>{{ $invoice->id }}-{{ $invoice->customer->company }}</title>
     
     <link href="/css/invoice.css" rel="stylesheet" />
 
